44ca42ddc45ef5d596473abb7ec7db08367a40df
Kenneth Spencer <ken@smallboxcms.com>
2013-03-09 21:45:23 -0800

    - pays attention to the form novalidate attribute

--- plugins/errors.js
+++ plugins/errors.js
@@ -12,16 +12,18 @@ function errorHandler(jqueryForm)
     this.form = jqueryForm.form;
 
     // Form is set not to validate
-    if (this.form.prop('noValidate')) {
+/*
+    if (this.form.prop('noValidate') || this.form.attr('novalidate')) {
         return;
     }
+*/
 
     var self = this;
     this._validities = [];
 
     this.submitPressed = false;
     
-    var messages = {
+    this.messages = {
         'required' : 'Please fill out this field',
         'email' : 'Please provide a valid email address',
         'date' : 'Date must be in YYYY-MM-DD Format',
@@ -56,117 +58,6 @@ function errorHandler(jqueryForm)
         }
     });
 
-    this.checkValidity = function(input)
-    {
-    /*
-        // Has Native Support
-        if (this.validity) {
-            var validity = this.validity;        
-        } else {
-            var validity = {
-                customError : false,
-                patternMismatch : false,
-                rangeOverflow : false,
-                rangeUnderflow : false,
-                stepMismatch : false, 
-                tooLong : false,
-                typeMismatch : false,
-                valid : true,
-                valueMissing : false
-            }
-
-            this.validity = validity;
-        }
-
-        */
-
-    /*
-        if (!input.name) {
-            return true;
-        }
-    */
-
-        if ($(input).attr('novalidate')) {
-            return true;        
-        }
-            
-        var value = this.getValue(input);
-
-        var isValid = true;
-        var type = 'unknown';
-
-        var hasValidity = false;
-        for (var i = 0, validity; validity = this._validities[i]; i++) {
-            var valid = validity.callback.call(this, input, value);
-
-            hasValidity |= valid !== null;
-
-            if (valid === true) {
-                this.removeValidity(input);
-            } else if (valid === false){
-                this.setValidity(input, validity.message, validity.name);
-                isValid = false;
-                break;
-            }
-        }
-    
-        if (hasValidity) {
-            $(input).addClass('has-validity');        
-        } 
-
-        if (isValid == true && input.checkValidity && input.checkValidity() == false) {
-            isValid = false;
-            var validity = input.validity;
-
-            if (validity.patternMismatch) {
-                type = 'pattern';
-            } else if (validity.valueMissing) {
-                type = 'required';
-            } else if (validity.typeMismatch) {
-                type = input.type;
-            }
-            
-            var message = $(input).data('error-message');
-
-            if (!message) {
-                message = messages[type] ? messages[type] : 'Please fill out this field correctly';
-            }
-
-            $(input).data('_error_type', type);
-            $(input).data('_error_message', message);
-        }
-
-        // to handle fileds linke radio buttons that have multiple inputs with same name
-        if (input.name) {
-            var list = $(':input[name="' + input.name + '"]', input.form);
-        } else {
-            var list = $(input);
-        }
-
-        if (isValid == true) {
-            if ($(input).hasDialog('#form-error-dialog')) {
-                $(input).dialogClose();
-            }
-            list.removeClass('user-error invalid');
-            list.addClass('valid');
-
-            return true;
-        } else {
-            // Manually Trigger oninvalid event for browsers that don't support it
-            if (false == self.hasOnInvalid) {
-                list.trigger('invalid');
-            }
-
-            list.removeClass('valid');
-            list.addClass('invalid');
-
-            if (this.submitPressed || $(input).data('userInteraction')) {
-                list.addClass('user-error');
-            }
-
-            return false;        
-        }
-    };
 
     // Browser does not support oninvalid polyfill it
     this.form.on('input keyup click', ':input', function(evt)
@@ -189,32 +80,7 @@ function errorHandler(jqueryForm)
         self.checkValidity(this);
     });
 
-    /* Safari has partial support for HTML5 validation. This allows us to test for actual support of the
-    *  oninvalid method so we can trigger it manually. 
-    */
-    var supportsValidity = function()
-    {
-        var supported = false;
-        var form = $('<form id="fake-form" style="display: none;"><input required="required"/><button type="submit"></form>').appendTo(document.body);
-
-        $('input', form).on('invalid', function(evt)
-        {
-            supported = true;
-            evt.preventDefault();
-        });
-
-        form.on('submit', function(evt)
-        {
-            evt.preventDefault();
-        });
-
-        $('button', form).trigger('click');
-        form.remove();
-
-        return supported;
-    }
-    this.hasOnInvalid = supportsValidity();
-
+    this.hasOnInvalid = this.supportsValidity();
 
     // Required Error for older browsers
     this.addCustomValidity('required', function(input, value)
@@ -230,7 +96,7 @@ function errorHandler(jqueryForm)
             return true;
         }
 
-    }, messages.required);
+    }, this.messages.required);
 
     // Email Matching for browsers that don't support it
     this.addCustomValidity('email', function(input, value)
@@ -249,7 +115,7 @@ function errorHandler(jqueryForm)
             return true;
         }
 
-    }, messages.email);
+    }, this.messages.email);
 
     // Pattern Matching for browsers that don't support it
     this.addCustomValidity('pattern', function(input, value)
@@ -271,7 +137,7 @@ function errorHandler(jqueryForm)
             return true;
         }
 
-    }, messages.pattern);
+    }, this.messages.pattern);
 
     // Value must match value of a different field
     this.addCustomValidity('match', function(input, value)
@@ -321,15 +187,160 @@ function errorHandler(jqueryForm)
         } else {
             return true;
         }
-    }, messages.date);
+    }, this.messages.date);
 
 
     this.validate();
 
 };
 
+/* Safari has partial support for HTML5 validation. This allows us to test for actual support of the
+*  oninvalid method so we can trigger it manually. 
+*/
+errorHandler.prototype.supportsValidity = function()
+{
+    var supported = false;
+    var form = $('<form id="fake-form" style="display: none;"><input required="required"/><button type="submit"></form>').appendTo(document.body);
+
+    $('input', form).on('invalid', function(evt)
+    {
+        supported = true;
+        evt.preventDefault();
+    });
+
+    form.on('submit', function(evt)
+    {
+        evt.preventDefault();
+    });
+
+    $('button', form).trigger('click');
+    form.remove();
+
+    return supported;
+}
+
+errorHandler.prototype.checkValidity = function(input)
+{
+    if (this.form.prop('noValidate') || this.form.attr('novalidate')) {
+        return;
+    }
+
+/*
+    // Has Native Support
+    if (this.validity) {
+        var validity = this.validity;        
+    } else {
+        var validity = {
+            customError : false,
+            patternMismatch : false,
+            rangeOverflow : false,
+            rangeUnderflow : false,
+            stepMismatch : false, 
+            tooLong : false,
+            typeMismatch : false,
+            valid : true,
+            valueMissing : false
+        }
+
+        this.validity = validity;
+    }
+
+*/
+
+/*
+    if (!input.name) {
+        return true;
+    }
+*/
+
+    if ($(input).attr('novalidate')) {
+        return true;        
+    }
+            
+    var value = this.getValue(input);
+
+    var isValid = true;
+    var type = 'unknown';
+
+    var hasValidity = false;
+    for (var i = 0, validity; validity = this._validities[i]; i++) {
+        var valid = validity.callback.call(this, input, value);
+
+        hasValidity |= valid !== null;
+
+        if (valid === true) {
+            this.removeValidity(input);
+        } else if (valid === false){
+            this.setValidity(input, validity.message, validity.name);
+            isValid = false;
+            break;
+        }
+    }
+    
+    if (hasValidity) {
+        $(input).addClass('has-validity');        
+    } 
+
+    if (isValid == true && input.checkValidity && input.checkValidity() == false) {
+        isValid = false;
+        var validity = input.validity;
+
+        if (validity.patternMismatch) {
+            type = 'pattern';
+        } else if (validity.valueMissing) {
+            type = 'required';
+        } else if (validity.typeMismatch) {
+            type = input.type;
+        }
+            
+        var message = $(input).data('error-message');
+
+        if (!message) {
+            message = this.messages[type] ? this.messages[type] : 'Please fill out this field correctly';
+        }
+
+        $(input).data('_error_type', type);
+        $(input).data('_error_message', message);
+    }
+
+    // to handle fileds linke radio buttons that have multiple inputs with same name
+    if (input.name) {
+        var list = $(':input[name="' + input.name + '"]', input.form);
+    } else {
+        var list = $(input);
+    }
+
+    if (isValid == true) {
+        if ($(input).hasDialog('#form-error-dialog')) {
+            $(input).dialogClose();
+        }
+    list.removeClass('user-error invalid');
+    list.addClass('valid');
+
+        return true;
+    } else {
+        // Manually Trigger oninvalid event for browsers that don't support it
+        if (false == self.hasOnInvalid) {
+            list.trigger('invalid');
+        }
+
+        list.removeClass('valid');
+        list.addClass('invalid');
+
+        if (this.submitPressed || $(input).data('userInteraction')) {
+            list.addClass('user-error');
+        }
+
+        return false;        
+    }
+}
+
 errorHandler.prototype.validate = function()
 {
+    if (this.form.prop('noValidate') || this.form.attr('novalidate')) {
+        return true;
+    }
+
     var self = this;
     var valid = true;
     var checked = {};
@@ -438,6 +449,10 @@ jQuery.fn.checkValidity = function(trigger, focus)
     var node = this.first();
     var form = node.prop('nodeName') == 'FORM' ? node : node.closest('form');
 
+    if (form.prop('noValidate') || form.attr('novalidate')) {
+        return true;
+    }
+
     var self = form.data('jqueryForm');
 
     if (!self) {
@@ -463,7 +478,6 @@ jQuery.fn.checkValidity = function(trigger, focus)
             }
         }
     }
-
 
     return retval;    
 }
